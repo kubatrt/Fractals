029a82d9b0c9736176d9e3808834ede73cf197cd
Jakub <kubatrt@gmail.com>
2015-02-07 18:03:19 +0100

    Initial
    basic fractal generation, free fly camera

---
+++ Assets/FlyCamera.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyCamera : MonoBehaviour 
+{
+	public GameObject defaultCam;
+	public GameObject targetObject;
+
+	public float flySpeed = 0.5f;
+	public float rotateSpeed = 2f;
+
+	bool isEnabled;
+	//float accelerationAmount = 3f;
+	float accelerationRatio = 1f;
+	float slowDownRatio = 0.5f;
+
+	void Start()
+	{
+		if(defaultCam != null) {
+			defaultCam.gameObject.camera.enabled = false;
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+		{
+			flySpeed *= accelerationRatio;
+		}
+		
+		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+		{
+			flySpeed /= accelerationRatio;
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+		{
+			flySpeed *= slowDownRatio;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
+		{
+			flySpeed /= slowDownRatio;
+		}
+
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			transform.Translate(defaultCam.transform.forward * flySpeed * Input.GetAxis("Vertical"));
+		}
+		if (Input.GetAxis("Horizontal") != 0)
+		{
+			transform.Translate(defaultCam.transform.right * flySpeed * Input.GetAxis("Horizontal"));
+		}
+
+		if (Input.GetKey(KeyCode.C))
+		{
+			transform.Translate(defaultCam.transform.up * flySpeed*0.5f);
+		}
+		else if (Input.GetKey(KeyCode.Z))
+		{
+			transform.Translate(-defaultCam.transform.up * flySpeed*0.5f);
+		}
+		else if (Input.GetKey(KeyCode.Q))
+		{
+			transform.RotateAround (targetObject.transform.position, Vector3.up, -rotateSpeed );
+		}
+		else if (Input.GetKey(KeyCode.E))
+		{
+			transform.RotateAround( targetObject.transform.position, Vector3.up, rotateSpeed);
+		}
+
+		if (Input.GetKeyDown(KeyCode.F12))
+			switchCamera();
+		//Moves the player to the flycam's position. Make sure not to just move the player's camera.
+		if (Input.GetKeyDown(KeyCode.M))
+			targetObject.transform.position = transform.position; 
+	}
+
+	void switchCamera()
+	{
+		if(defaultCam == null) {
+			Debug.LogWarning("No default camera!");
+			return;
+		}
+
+		// TODO
+		//means it is currently disabled. code will enable the flycam. you can NOT use 'enabled' as boolean's name.
+		if (!isEnabled) 
+		{
+			transform.position = defaultCam.transform.position; //moves the flycam to the defaultcam's position
+			defaultCam.camera.gameObject.SetActive(false);
+			this.camera.gameObject.SetActive(true);
+			isEnabled = true;
+		}
+		//if it is not disabled, it must be enabled. the function will disable the freefly camera this time.
+		else
+		{
+			this.camera.gameObject.SetActive(false);
+			defaultCam.camera.gameObject.SetActive(true);
+			isEnabled = false;
+		}
+	}
+}
---
+++ Assets/Fractal.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+
+	public Mesh		mesh;
+	public Material material;
+	public float 	maxDepth	= 4;
+	[Range(0,1)] 
+	public float 	childScale;
+
+	private int depth;
+
+	private static int	objectCounter; 
+
+	private void Start () {
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		if(depth < maxDepth) {
+			StartCoroutine("CreateChild");
+		}
+		//Debug.Log ("# Object counter: " + (++objectCounter));
+	}
+
+	private void Initialize(Fractal parent, Vector3 direction) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		transform.parent = parent.transform;
+		childScale = parent.childScale;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
+	}
+
+	private IEnumerator CreateChild()
+	{
+		yield return new WaitForSeconds(0.2f);
+		new GameObject("Fractal child").AddComponent<Fractal>().Initialize(this, Vector3.up);
+		yield return new WaitForSeconds(0.2f);
+		new GameObject("Fractal child").AddComponent<Fractal>().Initialize(this, Vector3.right);
+		yield return new WaitForSeconds(0.2f);
+		new GameObject("Fractal child").AddComponent<Fractal>().Initialize(this, Vector3.left);
+		yield return new WaitForSeconds(0.2f);
+		new GameObject("Fractal child").AddComponent<Fractal>().Initialize(this, Vector3.down);
+		yield return new WaitForSeconds(0.2f);
+		new GameObject("Fractal child").AddComponent<Fractal>().Initialize(this, Vector3.forward);
+		yield return new WaitForSeconds(0.2f);
+		new GameObject("Fractal child").AddComponent<Fractal>().Initialize(this, Vector3.back);
+
+	}
+}
